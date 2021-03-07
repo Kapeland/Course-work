28f1df9e429f7659415ca5cebfd77367e8293684
Kapeland <76999821+Kapeland@users.noreply.github.com>
2021-03-07 22:27:42 +0300

    Added a project

---
+++ cmake-build-debug/CMakeFiles/Kursach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kursach.dir/main.c.obj"
+  "Kursach.exe"
+  "Kursach.exe.manifest"
+  "Kursach.lib"
+  "Kursach.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Kursach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,47 @@
+#include <GL/glut.h>
+#include <math.h>
+
+#define M_PI 3.14159265358979323846
+#define WindowSize 500
+
+void display() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	double cnt = 100;
+	double l = 60;
+	double a = acos(-1) * 2.0 / 50.0;
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1, 0, 1);
+	glVertex2f(250, 250); // вершина в центре круга
+	for(int i = 0; i < cnt; i++) {
+		glVertex2f(250 + cos(a * i) * l, 250 + sin(a * i) * l);
+	}
+	glEnd();
+	glutSwapBuffers();
+}
+
+void reshape() {
+	glViewport(0, 0, WindowSize, WindowSize);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, WindowSize, 0, WindowSize);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(WindowSize, WindowSize);
+	glutInitWindowPosition(550, 200);
+	glutCreateWindow("Checkers");
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+}
